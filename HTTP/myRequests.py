d7708d7ee9db28b2c76a230d5a90d1990a260499
Mle4nbly <mle4nbly@gmail.com>
2022-12-17 19:07:40 +0700

    no commit

---
+++ HTTP/myRequests.py
@@ -0,0 +1,31 @@
+import requests
+from pprint import pprint
+
+url = "https://akabab.github.io/superhero-api/api/all.json"
+resp = requests.get(url)
+
+def get_status_request():
+    pprint(resp.json())
+
+def find_id_superhero(hero_name):
+    for hero_info in resp.json():
+        if hero_info['name'] == hero_name:
+            pprint(f"{hero_name} ID - {hero_info['id']}")
+
+def comparision_of_superhero(powerstat):
+    id_list = [149,332,655]
+    attribute_dict = {}
+    for id in id_list:
+        for hero_info in resp.json():
+            if hero_info['id'] == id:
+                attribute_dict[hero_info['name']] = hero_info['powerstats'][powerstat]
+    for name in attribute_dict:
+        if attribute_dict[name] == max(attribute_dict.values()):
+            print(f'Наибольший показатель {powerstat} у {name} - {attribute_dict[name]}')
+
+if __name__ == '__main__':
+    get_status_request()
+    find_id_superhero('Thanos')
+    comparision_of_superhero('intelligence')
+
+
