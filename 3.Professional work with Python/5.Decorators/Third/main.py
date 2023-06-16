d62d85f76610a47f9b46daf68318bcd21861a4e7
mle4nbly <mle4nbly@gmail.com>
2023-06-16 19:00:16 +0700

    decorators

--- 3.Professional work with Python/5.Decorators/Third/main.py
+++ 3.Professional work with Python/5.Decorators/Third/main.py
@@ -1,6 +1,24 @@
 import types
+import os
+from datetime import datetime
 
+##Приложение generator. Тема:'4.Iterators, generators'
 
+def logger(old_function):
+
+    def new_function(*args, **kwargs):
+        result = old_function(*args, **kwargs)
+        with open(os.path.join(os.path.dirname(__file__),'main.log'), 'a', encoding='utf8') as log_file:
+            log_file.write(f'{datetime.now()}, {old_function.__name__}, {args}|{kwargs}, {result.__name__}\n')
+        return result
+
+    return new_function
+
+path = f'{os.path.dirname(__file__)}\\main.log'
+if os.path.exists(path):
+    os.remove(path)
+
+@logger
 def flat_generator(list_of_lists):
     nested_list_cursor = -1
     main_list_cursor = 0
